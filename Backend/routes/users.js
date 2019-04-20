3744aa5d4c0811a6256f29df90e9c11084353d87
Ahmed Nassar <Ahmednassar@gmail.com>
2019-04-20 19:49:39 +0200

    Add Delete employee Endpoint

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -14,43 +14,30 @@ router.get('/', async (req, res) => {
   res.send(user)
 });
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {  
   const { error } = validate(req.body); 
+  
   if (error) return res.status(400).send(error.details[0].message);
-
+  
   if (req.body.password != req.body.rePassword) return res.status(400).send('Password mismatch') //added
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password', 'phone']));
+  user = new User(_.pick(req.body, ['name', 'email', 'password', 'rePassword', 'phone']));
   // const salt = await bcrypt.genSalt(10);
   // user.password = await bcrypt.hash(user.password, salt);
-  console.log(user);
+  console.log(user)
   await user.save();
 
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+  res.send(_.pick(user, ['_id', 'username', 'email']));
 });
 
-router.put('/', async (req, res) => {
-  const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
-
-  if (req.body.password != req.body.rePassword) return res.status(400).send('Password mismatch') //added
-
-  let user = await User.findOneAndUpdate({ email: req.body.email },
-    { $set: _.pick(req.body, ['name', 'email', 'password', 'phone']) }, 
-    { new: true },
-  );
-  if (!user) return res.status(400).send('User not found.');
-
-  // const salt = await bcrypt.genSalt(10);
-  // user.password = await bcrypt.hash(user.password, salt);
-  console.log(user);
-  await user.save();
-
-  res.send(_.pick(user, ['_id', 'name', 'email']));
-});
-
-
+router.delete('/',async (req,res)=>{
+  email = req.body.email;
+  let user = await User.findOne({email :email});
+  if (!user) return res.status(404).send("Wrong E-mail..")
+  const result = await User.remove({email:email})
+  res.send(result);
+})
 module.exports = router; 
