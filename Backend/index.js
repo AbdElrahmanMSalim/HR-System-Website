dfbe0932c5861990d659b0cbfeb7ef910046db49
Ahmed Nassar <Ahmednassar@gmail.com>
2019-04-21 00:48:41 +0200

    Allow conncet from outside localhost

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 
 require('./startup/logging')();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }); 
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
