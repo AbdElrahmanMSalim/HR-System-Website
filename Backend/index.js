44f5dc699ad6d665e4b7cb33ee91c8af2f2cca32
abdelrahmanmsalim <abdelrahman.m.salim@gmail.com>
2019-05-20 01:38:02 +0200

    header

--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,7 @@ const app = express();
 
 require("./startup/logging")();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 });
 require("./startup/routes")(app);
