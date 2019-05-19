a0f4adb33a0fb30bfceb59253362dac4ad1b0608
abdelrahmanmsalim <abdelrahman.m.salim@gmail.com>
2019-05-20 01:23:44 +0200

    fixing reports and suggestions

--- Backend/middleware/HR.js
+++ Backend/middleware/HR.js
@@ -1,8 +1,6 @@
 module.exports = function(req, res, next) {
   // 401 Unauthorized
   // 403 Forbidden
-
-  //todo change this to role
   if (req.user.role !== "HR") return res.status(403).send("Access denied.");
 
   next();
